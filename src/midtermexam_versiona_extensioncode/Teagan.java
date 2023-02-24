4eaa76fd6eb41fe21115e7283f20d3936e77714d
gamer <gamer@10.16.18.69>
2023-02-24 12:06:43 -0500

    Added the new branch and the Teagan class.

---
+++ src/midtermexam_versiona_extensioncode/Teagan.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author gamer
+ */
+public class Teagan {
+    // Hello, I am Teagan Cheung, a software developer!
+    // I like watching romantic movies and talking long walks on the beach at sunset.
+}
